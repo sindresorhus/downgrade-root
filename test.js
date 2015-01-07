024c179990ed99c263414b4c4050969c21d74646
silverwind <silv3rwind@gmail.com>
2015-01-07 16:24:26 +0100

    Add linux and sudo support

--- test.js
+++ test.js
@@ -4,6 +4,12 @@ var downgradeRoot = require('./');
 
 test(function (t) {
 	var _ = process.getuid;
+	var DEFAULT_UID = {
+		darwin: 501,
+		freebsd: 1000,
+		linux: 1000,
+		sunos: 100
+	};
 
 	process.getuid = function () {
 		return 0;
@@ -13,6 +19,6 @@ test(function (t) {
 
 	process.getuid = _;
 
-	t.assert(process.getuid() === 501);
+	t.assert(process.getuid() === DEFAULT_UID[process.platform]);
 	t.end();
 });
